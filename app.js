c586f6f78d99ca3037e7066b2d382151123f5fbf
leogirardeau <leogir.pro@gmail.com>
2020-08-09 17:49:18 +0200

    image by url / not from file anymore

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 new hoverEffect({
     parent: document.querySelector(".distortion"),
     intensity: 0.2,
-    image1: "./cat1.jpg",
-    image2: "./cat2.jpg",
+    image1: "https://images.pexels.com/photos/3809072/pexels-photo-3809072.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+    image2: "https://images.pexels.com/photos/3809065/pexels-photo-3809065.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     angle: Math.PI / 4,
-    displacementImage: "./diss.png"
+    displacementImage: "https://raw.githubusercontent.com/robin-dela/hover-effect/master/images/stripe1.png"
 });
