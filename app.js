31db377654a8ef2acec167563a1c784964072c32
leogirardeau <leogir.pro@gmail.com>
2020-08-09 16:39:14 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    inteensity: 0.2,
+    image1: './cat1.jpg',
+    image2: './cat2.jpg',
+    displacementImage: ".diss.png"
+});
